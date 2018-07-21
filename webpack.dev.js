7e1a0d4f605bfa5ae11d523be9cea3bac1de92fe
Wihelm Siso <wsiso@Wil.local>
2018-07-20 21:07:40 -0300

    Webpack common file config added

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,10 @@
-// webpack v4
-require('dotenv').config();
-
-const path = require('path');
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+module.exports = merge(common, {
     devServer: {
         contentBase: './dist',
         headers: {
@@ -15,43 +14,10 @@ module.exports = {
         port: process.env.NODE_PORT,
         publicPath: '/assets/' // this allow to make public the assets folder
     },
-    entry: { main: './src/index.js' },
     // output app js
     output: {
         path: path.resolve(__dirname, process.env.ASSETS_OUTPUT_FOLDER),
         filename: 'js/bundle.js'
-    },
-    module: {
-        rules: [{
-            // Babel
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: [ 
-                'style-loader',
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'postcss-loader',
-                'sass-loader'
-            ]
-        },
-        { 
-            test: /\.(jpg|png)$/,
-            loader: [{
-                loader: 'url-loader',
-                options: {
-                    emitFile: false
-                }
-            }]
-        }]
-    },
-    node: {
-        fs: 'empty', // allow require js files
     },
     plugins: [
         // Load ExtractTextPlugin to be used on the rules and output app css
@@ -60,12 +26,5 @@ module.exports = {
         }),
         // Load Hot Module plugin to refresh the browser with any file change
         new webpack.HotModuleReplacementPlugin(),
-    ],
-    resolve: {
-        alias: {
-            // allow use bg img in sass files, example: background: url('')
-            assets: path.resolve(__dirname, 'src/assets')
-        }
-    }
-    
-};
+    ]
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,58 +1,15 @@
-// webpack v4
-require('dotenv').config();
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const merge = require('webpack-merge');
 const path = require('path');
 const WebpackMd5Hash = require('webpack-md5-hash'); // hash css files
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: { main: './src/index.js' },
+module.exports = merge(common, {
     // output app js
     output: {
         path: path.resolve(__dirname, process.env.ASSETS_OUTPUT_FOLDER),
         filename: 'js/bundle.[chunkhash].js' // to take the name of the index.js change for [name].[chunkhash].js
-    },
-    module: {
-        rules: [{
-            // Babel
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-                }, {
-                    loader: MiniCssExtractPlugin.loader
-                },{
-                loader: "css-loader", options: {
-                    sourceMap: true
-                }
-                }, {
-                loader: "sass-loader", options: {
-                    sourceMap: true
-                }
-            }]
-        },
-        { 
-            test: /\.(jpg|png)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 1000, // if less than 10 kb, add base64 encoded image to css if not reference to file in  assets/img/
-                    name: '[name].[ext]', // if more than 10 kb move to this folder in build using file-loader
-                    outputPath: 'img/', // destiny folder for the files that exceed the limit param, the roor folder will be the specified on the output
-                    publicPath: '../img/' // url that will be replace the path for the files that exceed the limit param
-                }
-            }]
-        }]
-    },
-    node: {
-        fs: 'empty', // allow require js files
     },
     plugins: [
         new CleanWebpackPlugin('dist/assets', {} ),
@@ -61,11 +18,5 @@ module.exports = {
             filename: 'css/style.[contenthash].css',
         }),
         new WebpackMd5Hash()
-    ],
-    resolve: {
-        alias: {
-            // allow use bg img in sass files, example: background: url('')
-            assets: path.resolve(__dirname, 'src/assets')
-        }
-    }
-};
+    ]
+});
