7e574d4b735eb94476625f79c91af9bd90fe9c54
Wihelm Siso <wsiso@Wil.local>
2018-11-16 11:02:26 -0300

    Pettier effect hahaa

--- src/js/test.js
+++ src/js/test.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
 
 (() => {
-  const message = new Date(Date.now()).getDay() > 5 ? 'Working on weekend?' : 'All set and running';
+  const message =
+    new Date(Date.now()).getDay() > 5
+      ? 'Working on weekend?'
+      : 'All set and running';
   const greeting = `Hi developer!\ntest.js file: jquery testing\n${message}`;
   // eslint-disable-next-line no-console
   console.log(greeting);
