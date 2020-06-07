c183615afa93d16f051a8a54c63e326ace8bc8ae
Wilhelm Siso <wilhelm@wilhelm-macbook-pro.local>
2020-06-07 19:30:30 -0300

    rules fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,12 @@ import $ from 'jquery'
 // Import Bootstrap JS
 import 'bootstrap/dist/js/bootstrap'
 
+// eslint-disable-next-line prettier/prettier
 (() => {
-  const message = new Date(Date.now()).getDay() > 5
-    ? 'Working on weekend?'
-    : 'All set and running'
+  const message =
+    new Date(Date.now()).getDay() > 5
+      ? 'Working on weekend?'
+      : 'All set and running'
   const greeting = `Hi developer!\ntest.js file: jquery testing\n${message}`
   // eslint-disable-next-line no-console
   console.log(greeting)
