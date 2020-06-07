5b1bbc907e6b1c2eb7b5a0f5a461894aed67c77d
Wilhelm Siso <wilhelm@wilhelm-macbook-pro.local>
2020-06-07 16:57:14 -0300

    eslint fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
 // Import JQuery
-import $ from 'jquery';
+import $ from 'jquery'
 // Import Bootstrap JS
-import 'bootstrap/dist/js/bootstrap';
-
+import 'bootstrap/dist/js/bootstrap'
 (() => {
   const message =
     new Date(Date.now()).getDay() > 5
       ? 'Working on weekend?'
-      : 'All set and running';
-  const greeting = `Hi developer!\ntest.js file: jquery testing\n${message}`;
-  console.clear();
+      : 'All set and running'
+  const greeting = `Hi developer!\ntest.js file: jquery testing\n${message}`
+  console.clear()
   // eslint-disable-next-line no-console
-  console.log(greeting);
-})($);
+  console.log(greeting)
+})($)
